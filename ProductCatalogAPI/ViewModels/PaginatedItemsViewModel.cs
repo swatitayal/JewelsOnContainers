6379cc44d4ae605bcd5edddbfe396ec0a17b657a
swatitayal <31080980+swatitayal@users.noreply.github.com>
2021-11-09 23:57:16 -0800

    MVC
    Dockers + MVC - Infrastructure, model & services folder

---
+++ ProductCatalogAPI/ViewModels/PaginatedItemsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ProductCatalogAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogAPI.ViewModels
+{
+    public class PaginatedItemsViewModel
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; } // Size that is returned back
+        public long Count { get; set; }
+        public IEnumerable<CatalogItem> Data { get; set; }
+        
+    }
+}
